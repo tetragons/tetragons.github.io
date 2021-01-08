6197d15a847fc50cf5602bf1bc20d7e85e8502a0
tetragons <77084033+tetragons@users.noreply.github.com>
2021-01-08 01:21:43 -0500

    Restructuring

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/123.png'){
-    myImage.setAttribute('src', 'images/456.png');
+  if(mySrc === '../images/123.png'){
+    myImage.setAttribute('src', '../images/456.png');
   } else {
-      myImage.setAttribute('src', 'images/123.png');
+      myImage.setAttribute('src', '../images/123.png');
   }
 }
